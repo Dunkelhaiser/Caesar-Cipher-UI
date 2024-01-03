0148c37d4cdbe4145a10d18790ea5a2e0abfc8fc
KaeserOfHonour <103267805+KaeserOfHonour@users.noreply.github.com>
2024-01-03 15:53:38 +0200

    Added test framework

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,21 @@
+/// <reference types="vitest" />
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
     plugins: [react()],
+    test: {
+        globals: true,
+        clearMocks: true,
+        coverage: {
+            include: ["src/**/*"],
+            exclude: ["src/main.tsx", "src/vite-env.d.ts", "src/**/*.tsx"],
+            thresholds: {
+                "100": true,
+            },
+            provider: "v8",
+            reporter: ["text", "html", "json"],
+            reportsDirectory: "coverage",
+        },
+    },
 });
